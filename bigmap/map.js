fb448f344f186d23159c6c8156fab5a5e032c2aa
not_a_cat <fancy.zanta@gmail.com>
2018-05-19 12:49:10 +0300

    Bigmap X-stands highlight

--- bigmap/map.js
+++ bigmap/map.js
@@ -5,33 +5,35 @@ function mapGen() {
 	while (map.firstChild) map.removeChild(map.firstChild);
 
 	for (i = 0; i < coords.length; i++) {
+		var newMarkText = coords[i][0];
 		var newMarkX = (coords[i][1]-fixx)*modx+offx;
 		var newMarkY = (coords[i][2]-fixy)*mody+offy;
-		mapAddMark(newMarkX, newMarkY, 5);
-	}
-
-	for (i = 0; i < coords.length; i++) {
-		var newMarkX = (coords[i][1]-fixx)*modx+offx;
-		var newMarkY = (coords[i][2]-fixy)*mody+offy/*+(i%2)*5-3*/;
-		mapAddText(newMarkX, newMarkY, coords[i][0]);
+		mapAddMark(newMarkText, newMarkX, newMarkY, 5);
 	}
 }
 
-function mapAddMark (mx, my, r)
+function mapAddMark (mt, mx, my, r)
 {
 	var rect_mark = document.createElementNS(ns, 'rect');
+	if (mt.indexOf("X") !== -1) {
+		rect_mark.setAttribute('class', 'hili');
+		r+=1;
+	}
 	rect_mark.setAttribute('x', Math.round(mx-r));
 	rect_mark.setAttribute('y', Math.round(my-r));
 	rect_mark.setAttribute('width', r*2);
 	rect_mark.setAttribute('height', r*2);
 	map.appendChild(rect_mark);
+
+	var mark = document.createElementNS(ns, 'text');
+	mark.setAttribute('x', Math.round(mx));
+	mark.setAttribute('y', Math.round(my));
+	mark.appendChild(document.createTextNode(mt));
+	if (mt.indexOf("X") !== -1) mark.setAttribute('class', 'hili-text');
+	map.appendChild(mark);
 }
 
 function mapAddText (mx, my, p4)
 {
-	var mark = document.createElementNS(ns, 'text');
-	mark.setAttribute('x', Math.round(mx));
-	mark.setAttribute('y', Math.round(my));
-	mark.appendChild(document.createTextNode(p4));
-	map.appendChild(mark);
+
 }
